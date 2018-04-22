b3669c8711d88e39a8cb2e8db36170dee03a487f
Oliver Kobák <oliverkobak@Olivers-iMac.local>
2018-04-22 13:55:15 +0200

    TEST COMMIT

--- test.py
+++ test.py
@@ -5,4 +5,4 @@ import mysql.connector
 
 data = GetData()
 #data.downloadData("https://www.imf.org/external/pubs/ft/weo/2017/01/weodata/WEOApr2017all.xls","test_data.xls")
-data.importFileToDb("test_data.xls")
+# data.importFileToDb("test_data.xls")
